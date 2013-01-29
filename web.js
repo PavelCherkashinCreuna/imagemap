f27503c755a63cbe107c9cc6bf7438aced518c9c
Pavel Cherkashin <pcherk86@gmail.com>
2013-01-29 17:01:48 +0200

    added init

--- web.js
+++ web.js
@@ -30,4 +30,6 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
